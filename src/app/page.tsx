6fe8443514270fdbe9afa9d7ea904a4db336913c
Rachit Srivastava <honey032004@gmail.com>
2025-04-06 21:53:59 +0530

    feat: added menubar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,36 @@
 import Image from "next/image";
+import {
+  Menubar,
+  MenubarMenu,
+  MenubarTrigger,
+} from "@/components/ui/menubar"
 
 export default function Home() {
   return (
-      <div>
+    <div>
+      <div className='flex justify-between'>
         <Image
-            className="m-2"
+            className="m-3 relative"
             src="/club_logo.png"
-            width={80}
-            height={80}
+            width={65}
+            height={65}
             alt="Picture of the author"
         />
-        <p className="font-bold m-1">Torvalds Club</p>
+<div className='mt-10 mr-10 flex justify-center gap-2'>
+      
+<Menubar>
+  <MenubarMenu>
+    <MenubarTrigger>Home</MenubarTrigger>
+    <MenubarTrigger>Events</MenubarTrigger>
+    <MenubarTrigger>Gallery</MenubarTrigger>
+    <MenubarTrigger>Other Clubs</MenubarTrigger>
+    <MenubarTrigger>Contact Us</MenubarTrigger>
+  </MenubarMenu>
+</Menubar>
+
+</div>
+      </div>
+      <p className="font-bold ml-1">Torvalds Club</p>
       </div>
   );
 }
